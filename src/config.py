b7a4a405ba5f236590f9521e1b4706825e527151
saadahmed313 <89286569+saadahmed313@users.noreply.github.com>
2025-06-15 00:19:15 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,5 @@
-import os
 from pathlib import Path
 
-class Config:
-    BASE_DIR = Path.home() / "Desktop" / "tjm-project"
-    API_URL = "https://jsonplaceholder.typicode.com/posts"
-    NUM_POSTS = 10
-    TYPE_INTERVAL = 0.05
-    PAUSE_BETWEEN_ACTIONS = 0.5
+SAVE_DIR = Path.home() / "Desktop" / "tjm-project"
+API_URL = "https://jsonplaceholder.typicode.com/posts"
+NUM_POSTS = 10
